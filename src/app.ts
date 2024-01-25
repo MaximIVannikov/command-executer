38fdc2411c1db50ffd50a9ed099b290ae30797aa
Maxim <ivannikov.maxim1@gmail.com>
2024-01-25 15:46:44 +0100

    handlers, prompt & logger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+import { PromptService } from './core/prompt/prompt.service';
+
 export class App {
-	run() {
-		console.log('run');
+	async run() {
+		const res = await new PromptService().input<number>('number', 'number');
+		console.log(res);
 	}
 }
 const app = new App();
